e836dc801df0bf3a73cb597c83ac77b636e138f2
amitkain7 <kainturagolu688@gmail.com>
2025-03-18 19:43:54 +0530

    all done

--- src/App.jsx
+++ src/App.jsx
@@ -1,23 +1,23 @@
-import React from 'react'
-import Login from './components/Login'
-import {Routes , Route} from 'react-router-dom'
-import Register from './components/Register'
-import Home from './components/Home'
-import Job from './components/job'
-import SingleJob from './components/SingleJob'
+import React from "react";
+import Login from "./components/Login";
+import { Routes, Route } from "react-router-dom";
+import Register from "./components/Register";
+import Home from "./components/Home";
+import Job from "./components/Job";
+import SingleJob from "./components/SingleJob";
 
 const App = () => {
   return (
     <div>
       <Routes>
-        <Route path='/' element={<Home/>}/>
-        <Route path='/job' element={<Job/>}/>
-        <Route path={`/singlejob/:id`} element={<SingleJob/>}/>
-        <Route path='/login' element={<Login/>}/> 
-        <Route path='/register' element={<Register/>}/>
+        <Route path="/" element={<Home />} />
+        <Route path="/job" element={<Job />} />
+        <Route path={`/singlejob/:id`} element={<SingleJob />} />
+        <Route path="/login" element={<Login />} />
+        <Route path="/register" element={<Register />} />
       </Routes>
     </div>
-  )
-}
+  );
+};
 
-export default App
+export default App;
